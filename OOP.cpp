ba7d4a9947fe8e155084e6c89da4c78fe9751582
Nrigot <chernobaystas@gmail.com>
2026-08-25 20:40:39 +0300

    First Exersice

--- OOP.cpp
+++ OOP.cpp
@@ -1,65 +1,175 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <cstring>
+#include <string.h>
 #include <windows.h>
 using namespace std;
 
-#define T 20
+#define T 30
 
-class firstclass_MyPoint {
+class student {
 
-public: 
-	const char* getName()const { return (strcmp(name, "") == 0) ? "Point" : name; }
-	int getX()const { return x; }
-	int getY()const { return y; } // это аксессоры, которые позволяют получить значения закрытых переменных x и y. Они объявлены как константные методы, что означает, что они не изменяют состояние объекта.
 
-	void setName(const char* newName) { strncpy_s(name, T, newName, T - 1); name[T - 1] = '\0'; } // это мутаторы, которые позволяют установить значения закрытых переменных name, x и y. Они принимают параметры и изменяют состояние объекта.
-	void setX(int newX); // объявляем сеттеры в классе
-	void setY(int newY);
+public:
+	int IS_Student(const char* name_of_study_place) {
+		if (name_of_study_place == nullptr || strcmp(name_of_study_place, "Don't study at any place") == 0) {
+			cerr << "Помилка: студент не навчається ніде!" << endl;
+			return 0;
+		}
+		cout << "Це студент" << endl;
+		return 1;
+	}
+
+    void InputInfo();
+    void ShowInfo() const;
+
+
+    // --- Геттери ---
+    inline const char* Get_Name_of_study_place() const;
+    inline const char* Get_name() const;
+    inline const char* Get_surname() const;
+    inline const char* Get_lastName() const;
+    inline int Get_date_of_birth() const;
+    inline int Get_phone() const;
+    inline const char* Get_native_city() const;
+    inline const char* Get_native_country() const;
+    inline int Get_number_of_the_group() const;
+
+    // --- Сеттери ---
+    inline int Set_Name_of_study_place(const char* Name_of_study_place);
+    inline int Set_name(const char* name);
+    inline int Set_surname(const char* surname);
+    inline int Set_lastName(const char* lastName);
+    inline int Set_date_of_birth(int date_of_birth);
+    inline int Set_phone(int phone);
+    inline int Set_native_city(const char* native_city);
+    inline int Set_native_country(const char* native_country);
+    inline int Set_number_of_the_group(int number_of_the_group);
+    
 
 private:
-	char name[T]{"Point"};
-	int x{0};
-	int y{0};
+    char Name_of_study_place[T]{ "Don't study at any place" };
+    char name[T]{ "No name" };
+    char surname[T]{ "No surname" };
+    char lastName[T]{ "No last name" };
+    int date_of_birth{ 0 };
+    int phone{ 0 };
+    char native_city[T]{ "Without native city" };
+    char native_country[T]{ "Without native country" };
+    int number_of_the_group{ 0 };
 
 };
 
 
-void firstclass_MyPoint::setX(int newX) { 
-	this->x = newX; }
-void firstclass_MyPoint::setY(int newY) { this->y = newY; }
+// --- Реалізація геттерів ---
+const char* student::Get_Name_of_study_place() const { return Name_of_study_place; }
+const char* student::Get_name() const { return name; }
+const char* student::Get_surname() const { return surname; }
+const char* student::Get_lastName() const { return lastName; }
+int student::Get_date_of_birth() const { return date_of_birth; }
+int student::Get_phone() const { return phone; }
+const char* student::Get_native_city() const { return native_city; }
+const char* student::Get_native_country() const { return native_country; }
+int student::Get_number_of_the_group() const { return number_of_the_group; }
 
-struct firstclass_MyPoint1 {
-    char name[T]{"Point"};
-    int x{0};
-    int y{0};
+// --- Реалізація сеттерів ---
+int student::Set_Name_of_study_place(const char* Name_of_study_place) {
+    strcpy_s(this->Name_of_study_place, Name_of_study_place);
+    return 0;
+}
+int student::Set_name(const char* name) {
+    strcpy_s(this->name, name);
+    return 0;
+}
+int student::Set_surname(const char* surname) {
+    strcpy_s(this->surname, surname);
+    return 0;
+}
+int student::Set_lastName(const char* lastName) {
+    strcpy_s(this->lastName, lastName);
+    return 0;
+}
+int student::Set_date_of_birth(int date_of_birth) {
+    this->date_of_birth = date_of_birth;
+    return 0;
+}
+int student::Set_phone(int phone) {
+    this->phone = phone;
+    return 0;
+}
+int student::Set_native_city(const char* native_city) {
+    strcpy_s(this->native_city, native_city);
+    return 0;
+}
+int student::Set_native_country(const char* native_country) {
+    strcpy_s(this->native_country, native_country);
+    return 0;
+}
+int student::Set_number_of_the_group(int number_of_the_group) {
+    this->number_of_the_group = number_of_the_group;
+    return 0;
+}
 
-};
+
+
+void student::InputInfo() {
+    cout << "Введіть ім'я: ";
+    cin.getline(name, T);
+
+    cout << "Введіть прізвище: ";
+    cin.getline(surname, T);
+
+    cout << "Введіть по батькові: ";
+    cin.getline(lastName, T);
+
+    cout << "Введіть дату народження (рік): ";
+    cin >> date_of_birth;
+
+    cout << "Введіть номер телефону: ";
+    cin >> phone;
+    cin.ignore();
+
+    cout << "Введіть місто: ";
+    cin.getline(native_city, T);
+
+    cout << "Введіть країну: ";
+    cin.getline(native_country, T);
+
+    cout << "Введіть навчальний заклад: ";
+    cin.getline(Name_of_study_place, T);
+
+    cout << "Введіть номер групи: ";
+    cin >> number_of_the_group;
+    cin.ignore();
+
+    cout << "\n Дані успішно введено!\n";
+}
+
+void student::ShowInfo() const {
+    cout << "\n--- Інформація про студента ---\n";
+    cout << "Ім'я: " << name << endl;
+    cout << "Прізвище: " << surname << endl;
+    cout << "По батькові: " << lastName << endl;
+    cout << "Дата народження: " << date_of_birth << endl;
+    cout << "Телефон: " << phone << endl;
+    cout << "Місто: " << native_city << endl;
+    cout << "Країна: " << native_country << endl;
+    cout << "Навчальний заклад: " << Name_of_study_place << endl;
+    cout << "Номер групи: " << number_of_the_group << endl;
+}
 
 int main()
 { 
-    
-    //инкапсуляция - скрытие внутреннего представления объекта. полиморфизм - способность объекта иметь разные формы. наследование - механизм создания новых классов на основе существующих. абстракция - выделение основных характеристик объекта.
-
-	// private public protected - это модификаторы доступа в C++. Они определяют, какие члены класса (переменные и функции) могут быть доступны из других частей программы.
-	// private - члены класса доступны только внутри самого класса. public - члены класса доступны из любого места программы. protected - члены класса доступны внутри самого класса и его наследников.
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-	firstclass_MyPoint point1;
-	point1.setName("MyPoint");
-	point1.setY(T);
-	if(point1.getX() == 0) {
-		point1.setX(15);
-	}
 
-	cout << "Name: " << point1.getName() << endl; 
-	cout << "X: " << point1.getX() << endl; 
-	cout << "Y: " << point1.getY() << endl; 
+    student s;
+    s.InputInfo();
+    s.ShowInfo();
 
-	firstclass_MyPoint1 point2;
-    point2.x = 10;
 
+	s.IS_Student(s.Get_Name_of_study_place());
+    return 0;
 
 
 }

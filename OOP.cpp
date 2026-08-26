5d956b0de9e768e39af14d7988d4ffa80d989e81
Nrigot <chernobaystas@gmail.com>
2026-08-26 17:27:23 +0300

    Homework final part

--- OOP.cpp
+++ OOP.cpp
@@ -6,61 +6,127 @@ using namespace std;
 
 #define T 20
 
-class firstclass_MyPoint {
+class book_of_phones {
 
-public: 
-	const char* getName()const { return (strcmp(name, "") == 0) ? "Point" : name; }
-	int getX()const { return x; }
-	int getY()const { return y; } // это аксессоры, которые позволяют получить значения закрытых переменных x и y. Они объявлены как константные методы, что означает, что они не изменяют состояние объекта.
+	public:
+		inline void EditInfo(int choice);
+        // --- Конструктор по умолчанию ---
+        book_of_phones() : Phone_number(0) {
+            strcpy_s(Name, T, "No name");
+            strcpy_s(Second_name, T, "No second name");
+            strcpy_s(Last_name, T, "No last name");
 
-	void setName(const char* newName) { strncpy_s(name, T, newName, T - 1); name[T - 1] = '\0'; } // это мутаторы, которые позволяют установить значения закрытых переменных name, x и y. Они принимают параметры и изменяют состояние объекта.
-	void setX(int newX); // объявляем сеттеры в классе
-	void setY(int newY);
+            additional_info = new char[T];
+            strcpy_s(additional_info, T, "No additional info");
+        }
 
-private:
-	char name[T]{"Point"};
-	int x{0};
-	int y{0};
+        book_of_phones(const book_of_phones& other)
+            : Phone_number(other.Phone_number)
+        {
+            strcpy_s(Name, T, other.Name);
+            strcpy_s(Second_name, T, other.Second_name);
+            strcpy_s(Last_name, T, other.Last_name);
 
+            additional_info = new char[strlen(other.additional_info) + 1];
+            strcpy_s(additional_info, strlen(other.additional_info) + 1, other.additional_info);
+        }
+
+
+        // --- Деструктор ---
+        ~book_of_phones() {
+            delete[] additional_info;
+            additional_info = nullptr;
+        }
+
+		inline void Get_info() {
+            cout << "Введіть ім'я: ";
+            cin.getline(Name, T);
+			cout << "Name: " << Name << endl;
+            cout << "Введітьпрізвище: ";
+            cin.getline(Second_name, T);
+			cout << "Second name: " << Second_name << endl;
+            cout << "Введіть по батькові: ";
+            cin.getline(Last_name, T);
+			cout << "Last name: " << Last_name << endl;
+            cout << "Введіть номер телефону: ";
+            cin >> Phone_number;
+			cout << "Phone number: " << Phone_number << endl;
+            cout << "Введіть додаткову інформацію: ";
+            cin.getline(additional_info, T);
+			cout << "Additional info: " << additional_info << endl;
+		}
+
+		inline  void Set_info(const char* name, const char* second_name, const char* last_name, int phone_number, const char* additional_info_str) {
+			strncpy_s(Name, T, name, T - 1);
+			strncpy_s(Second_name, T, second_name, T - 1);
+			strncpy_s(Last_name, T, last_name, T - 1);
+			Phone_number = phone_number;
+			delete[] additional_info; // Free the old memory
+			additional_info = new char[strlen(additional_info_str) + 1];
+			strcpy_s(additional_info, strlen(additional_info_str) + 1, additional_info_str);
+		}
+
+	private:
+		char Name[T]{"No name"};
+		char Second_name[T]{"No second name"};
+		char Last_name[T]{"No last name"};
+		int Phone_number{ 0 };
+		char* additional_info{ nullptr };
 };
 
+void book_of_phones::EditInfo(int choice) {
+    switch (choice) {
+    case 1:
+        cout << "Введіть нове ім'я: ";
+        cin.getline(Name, T);
+        break;
+    case 2:
+        cout << "Введіть нове прізвище: ";
+        cin.getline(Second_name, T);
+        break;
+    case 3:
+        cout << "Введіть нове по батькові: ";
+        cin.getline(Last_name, T);
+        break;
+    case 4:
+        cout << "Введіть новий номер телефону: ";
+        cin >> Phone_number;
+        cin.ignore();
+        break;
+    case 5:
+        cout << "Введіть нову додаткову інформацію: ";
+        {
+            char temp[T];
+            cin.getline(temp, T);
+            delete[] additional_info; // звільняємо стару пам'ять
+            additional_info = new char[strlen(temp) + 1];
+            strcpy_s(additional_info, strlen(temp) + 1, temp);
+        }
+        break;
+    default:
+        cout << "Невірний вибір!" << endl;
+        break;
+    }
+}
 
-void firstclass_MyPoint::setX(int newX) { 
-	this->x = newX; }
-void firstclass_MyPoint::setY(int newY) { this->y = newY; }
-
-struct firstclass_MyPoint1 {
-    char name[T]{"Point"};
-    int x{0};
-    int y{0};
-
-};
 
 int main()
 { 
-    
-    //инкапсуляция - скрытие внутреннего представления объекта. полиморфизм - способность объекта иметь разные формы. наследование - механизм создания новых классов на основе существующих. абстракция - выделение основных характеристик объекта.
-
-	// private public protected - это модификаторы доступа в C++. Они определяют, какие члены класса (переменные и функции) могут быть доступны из других частей программы.
-	// private - члены класса доступны только внутри самого класса. public - члены класса доступны из любого места программы. protected - члены класса доступны внутри самого класса и его наследников.
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-
-	firstclass_MyPoint point1;
-	point1.setName("MyPoint");
-	point1.setY(T);
-	if(point1.getX() == 0) {
-		point1.setX(15);
-	}
-
-	cout << "Name: " << point1.getName() << endl; 
-	cout << "X: " << point1.getX() << endl; 
-	cout << "Y: " << point1.getY() << endl; 
-
-	firstclass_MyPoint1 point2;
-    point2.x = 10;
-
-
+	book_of_phones phone1;
+    phone1.Get_info();
+    int choice;
+    cout << "Що хочете змінити?\n"
+        << "1 - Ім'я\n"
+        << "2 - Прізвище\n"
+        << "3 - По батькові\n"
+        << "4 - Телефон\n"
+        << "5 - Додаткова інформація\n"
+        << "Ваш вибір: ";
+    cin >> choice;
+    cin.ignore();
+    phone1.EditInfo(choice);
 
 }
 

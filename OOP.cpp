57cf8749b2c2f8c818f56a021ff27f7395cf2a23
Nrigot <chernobaystas@gmail.com>
2026-09-01 22:49:00 +0300

    baze

--- OOP.cpp
+++ OOP.cpp
@@ -6,6 +6,47 @@ using namespace std;
 
 #define T 30
 
+struct Date {
+	int day;
+	int month;
+	int year;
+
+	void input() {
+		cout << "Enter day: ";
+		cin >> day;
+		cout << "Enter month: ";
+		cin >> month;
+		cout << "Enter year: ";
+		cin >> year;
+	}
+
+	void output() {
+		cout << day << "/" << month << "/" << year;
+	}
+};
+
+class book {
+private:
+	char* author{ nullptr };
+	char* title{ nullptr };
+	char* publishing{ nullptr };
+	char* annotation{ nullptr };
+	int year{ 0 };
+	int counter{ 0 };
+	int pages{ 0 };
+	int annotation{ 0 };
+
+
+
+
+
+
+public:
+
+};
+
+
+
 
 
 int main()

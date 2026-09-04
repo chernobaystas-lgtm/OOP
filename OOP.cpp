d7e5c319c23e7fcc8f0835424f9c31049b18b57e
Nrigot <chernobaystas@gmail.com>
2026-09-04 20:20:05 +0300

    Final of tutorial

--- OOP.cpp
+++ OOP.cpp
@@ -8,111 +8,102 @@
 #include <stack>
 using namespace std;
 
+template <class T, int N>
+class MyStack;
 
-
-
-
-
-
-template <class T>
-
-class Stack {
-private:
+template <class T, int N>
+class Node {
+public:
 	T value;
-	Node<T>* next;
-	friend ostream& operator<<(ostream& s, const Stack<T>& next) {
-		s << value << endl;
+	Node<T, N>* next;
+
+	Node(T value) : value(value), next(nullptr) {}
+
+	friend ostream& operator<<(ostream& s, const Node<T, N>& n) {
+		s << n.value << endl;
 		return s;
 	}
-public:
 
-	Stack(T value) : value(value), next(nullptr) {}
-
-	friend class Mystack<T>;
+	friend class MyStack<T, N>;
 };
 
 
-template <class T>
+template <class T, int N>
 class MyStack {
 private:
-
-	Node<T> top{ nullptr };
-
+	Node<T, N>* top{ nullptr }; 
 
 public:
 	MyStack();
-	void push(const T& value); // push item to stack top
-	void pop();                // remove item from stack top
-	T& retTop() const;            // return stack top item
-	bool empty() const;        // if stack is empty
-	int size() const;          // stack size
+	void push(const T& value);
+	void pop();
+	T& retTop() const;
+	bool empty() const;
+	int size() const;
+	bool isFull()const;
 	~MyStack();
 	void clear();
 };
-template <class T>
-MyStack<T>::~MyStack<T>() {
 
-	/*Node<T>* temp;
-	while (top != nullptr) {
-		temp = top;
-		top = top->next;
-		delete temp;
-	}
-	top = nullptr;*/
+template <class T, int N>
+MyStack<T, N>::MyStack() {} 
+
+template <class T, int N>
+MyStack<T, N>::~MyStack() {
 	clear();
-
 }
-template <class T>
-void MyStack<T>::clear() {
 
-	Node<T>* temp;
+template <class T, int N>
+void MyStack<T, N>::clear() {
+	Node<T, N>* temp;
 	while (top != nullptr) {
 		temp = top;
 		top = top->next;
 		delete temp;
 	}
 	top = nullptr;
-
 }
 
-
-
-template <class T>
-void MyStack<T>::push(const T& value) {
-	Node<T>* temp = new Node<T>{value};
+template <class T, int N>
+void MyStack<T, N>::push(const T& value) {
+	assert(!isFull() && "Stack is full");
+	Node<T, N>* temp = new Node<T, N>(value);
 	temp->next = top;
 	top = temp;
 }
 
-template <class T>
-void MyStack<T>::pop() {
+template <class T, int N>
+void MyStack<T, N>::pop() {
 	if (!empty()) {
-		Node<T>* temp = top;
+		Node<T, N>* temp = top;
 		top = top->next;
 		delete temp;
-
 	}
 	else {
 		cout << "Stack is empty" << endl;
 	}
 }
 
-
-template <class T>
-T& MyStack<T>::retTop() const {
+template <class T, int N>
+T& MyStack<T, N>::retTop() const {
 	assert(top != nullptr && "Stack is empty");
 	return top->value;
 }
 
-template <class T>
-bool MyStack<T>::empty() const {
-	return(top == nullptr) ? true : false;
+template <class T, int N>
+bool MyStack<T, N>::empty() const {
+	return (top == nullptr) ? true : false;
 }
 
-template <class T>
-int MyStack<T>::size() const {
+template <class T, int N>
+bool MyStack<T, N>::isFull() const {
+	return (size() == N) ? true : false;
+}
+
+template <class T, int N>
+int MyStack<T, N>::size() const {
 	int count = 0;
-	Node<T>* current = top;
+	Node<T, N>* current = top;
 	while (current != nullptr) {
 		count++;
 		current = current->next;
@@ -121,11 +112,18 @@ int MyStack<T>::size() const {
 }
 
 int main()
-{ 
+{
+	char str[100]{ "lorem hi to u" };
+	cout << "String: " << str << endl;
+	MyStack<char, 100> myCharstack;
+	for (auto ch : str) {
+		myCharstack.push(ch);
+	}
 
-
-
-
+	cout << "Stack pop: ";
+	while (!myCharstack.empty()) {
+		cout << myCharstack.retTop() << " ";
+		myCharstack.pop();
+	}
+	system("pause");
 }
-
-
